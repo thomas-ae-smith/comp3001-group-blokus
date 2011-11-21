409c55da8d128acfff430cf2c3957070ac4bf469
Skinna <js40g09@ecs.soton.ac.uk>
2011-11-21 22:42:35 +0000

    Fixed and finalized datastore.

--- src/blokus/models.py
+++ src/blokus/models.py
@@ -1,31 +1,34 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, MaxLengthValidator, MinLengthValidator, RegexValidator
 from django.db import models
 
 class Game(models.Model):
 	start_time = models.DateTimeField()
 	game_type = models.IntegerField()
+	player_turn = models.PositiveIntegerField(validators=[MaxValueValidator(3)])
 
 class PieceMaster(models.Model):
 	piece_data = models.CharField(max_length=25)
 
-class Player(models.Model):
-	name = models.CharField(validators=[MinValueValidator(0), MaxValueValidator(30)])
+class UserProfile(models.Model):
+	name = models.CharField(max_length=30)
+
 	wins = models.IntegerField()
 	losses = models.IntegerField()
 
-class PositionColour(models.Model):
-	colour = models.CharField()
+_colour_regex = r"^(red|yellow|green|blue)$"
+
+class Player(models.Model):
+	game = models.ForeignKey(Game)
+	colour = models.CharField(max_length=6, validators=[RegexValidator(regex=_colour_regex)])
+	user = models.ForeignKey(UserProfile)
 
 class Piece(models.Model):
 	game = models.ForeignKey(Game)
 	master = models.ForeignKey(PieceMaster)
 	player = models.ForeignKey(Player)
-	x = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(20)])
-	y = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(20)])
-	rotation = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(3)])
-	flip = models.BooleanField()
 
-class PlayerGame(models.Model):
-	game = models.ForeignKey(Game)
-	index = models.ForeignKey(PositionColour)
-	player = models.ForeignKey(Player)
+	x = models.PositiveIntegerField(validators=[MaxValueValidator(20)],null=True)
+	y = models.PositiveIntegerField(validators=[MaxValueValidator(20)],null=True)
+
+	rotation = models.PositiveIntegerField(validators=[MaxValueValidator(3)])
+	flip = models.BooleanField()
