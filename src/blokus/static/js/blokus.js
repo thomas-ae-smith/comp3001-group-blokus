27ab73a243fc5546858ac184363239e93c6ccd49
Peter West <peter.g.west@gmail.com>
2011-11-21 22:49:19 +0000

    Added hack to check URL and render correct content. Added hard-coded example game collection. Ready to render game list for lobby.

--- src/blokus/static/js/blokus.js
+++ src/blokus/static/js/blokus.js
@@ -19,16 +19,32 @@ window.blokus = (function ($, _, Backbone, Raphael) {
 	document.createElement('aside');
 	
 	$(document).ready(function(){
-		// TEMP: Hard coded players
-		var players = new blokus.PlayerCollection([
-			{ name: "zanders3" },
-			{ name: "timzo" },
-			{ name: "bob" },
-			{ name: "superzico" }
-		]);
+		
 
-		var gameboard = new blokus.GameBoard({ players: players });
-		$("#container").append(gameboard.render().el);
+		// TEMP HACK
+		if (location.href.indexOf("game") > -1) {
+			
+
+		} else if (location.href.indexOf("lobby") > -1) {
+			var gameCollection = new blokus.GameCollection([
+				{ id: 0, name: "game 1", selected: true },
+				{ id: 1, name: "the colourblinds" },
+				{ id: 2, name: "battle of teh awsumz" }
+			]);
+			var gameList = new blokus.GameList({ gameCollection: gameCollection });
+			$("#lobbylist-container").append(gameList.render().el);
+		} else {
+			// TEMP: Hard coded players
+			var players = new blokus.PlayerCollection([
+				{ name: "zanders3" },
+				{ name: "timzo" },
+				{ name: "bob" },
+				{ name: "superzico" }
+			]);
+
+			var gameboard = new blokus.GameBoard({ players: players });
+			$("#container").append(gameboard.render().el);
+		}
 	});
 
 	return {
