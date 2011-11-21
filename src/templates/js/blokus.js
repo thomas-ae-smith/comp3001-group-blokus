03049ac34c80e4480409060e4533650928520701
Peter West <peter.g.west@gmail.com>
2011-11-21 15:24:33 +0000

    Rendering moved from game.html to blokus.js

--- src/templates/js/blokus.js
+++ src/templates/js/blokus.js
@@ -11,13 +11,37 @@ window.blokus = (function ($, _, Backbone, Raphael) {
 			player: "/api/rest/player/"
 		};
 
-	function createBoard() {
+	//Ensure HTML 5 elements are styled by IE
+	document.createElement('header');
+	document.createElement('nav');
+	document.createElement('section');
+	document.createElement('article');
+	document.createElement('aside');
+	
+	var drawPlayer = function(paper, x, y, name){
+		paper.rect(x, y, 250, 110, 5).attr("fill", "#E3EEF2");
+		paper.text(x + 8, y + 10, name).attr({
+			"text-anchor":"start",
+			"font-family":"Tahoma",
+			"font-weight":"bold",
+			"font-size":14
+		});
+	};
+	
+	$(document).ready(function(){
+		var paper = Raphael("gameboard", 800, 600);
+		var players = ["zanders3", "timzo", "bob", "superzico"];
 		
-	}
-
-	$(document).ready(function () {
+		//Player list with pieces
+		var y = 30;
+		for (var i in players) {
+			drawPlayer(paper, 10, y, players[i]);
+			y += 120;
+		}
 		
-		createBoard();
+		//Gameboard
+		paper.rect(290, 10, 510, 510, 5).attr("fill", "#GGGGGG");
+		paper.rect(295, 15, 500, 500).attr("fill", "#AAAAAA");
 	});
 
 	return {
