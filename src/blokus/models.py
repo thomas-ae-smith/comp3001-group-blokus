b4e97bbca8f654a4ea6d0cc6a36dba2165c8766f
Skinna <js40g09@ecs.soton.ac.uk>
2011-11-22 17:51:04 +0000

    Added skeletal functions for serverside people to implement

--- src/blokus/models.py
+++ src/blokus/models.py
@@ -6,14 +6,21 @@ class Game(models.Model):
 	game_type = models.IntegerField()
 	player_turn = models.PositiveIntegerField(validators=[MaxValueValidator(3)])
 
+	#def place_piece(self, piece):
+
 class PieceMaster(models.Model):
-	piece_data = models.CharField(max_length=25)
+	piece_data = models.CharField(max_length=12)	#Repretented by 'T', 'F' and ','; 'T' represents a block, 'F' represents no block, ',' represents newline.
+
+	#def get_bitmap(self, rot, flip):
 
 class UserProfile(models.Model):
 	name = models.CharField(max_length=30)
 
 	wins = models.IntegerField()
 	losses = models.IntegerField()
+
+	#def __init__(self, name, password):
+	#Currently accepts 'master' with any username so functions can be worked on before authentication is implemented.
 
 _colour_regex = r"^(red|yellow|green|blue)$"
 
@@ -32,3 +39,6 @@ class Piece(models.Model):
 
 	rotation = models.PositiveIntegerField(validators=[MaxValueValidator(3)])
 	flip = models.BooleanField()
+
+	#def flip(self, horizontal):	#Flips the piece horizontally; horizontal is a bool where T flips horizontally and F flips vertically.
+	#def rotate(self, clockwise):	#Rotates the piece clockwise; 'clockwise' should be a bool; T for clockwise rotation, F for anticlockwise.
