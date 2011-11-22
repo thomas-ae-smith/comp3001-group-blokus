3e6c1d4c69e0d7929e06d63dc819f358d879ec13
adamscybot <adamscybot@localhost>
2011-11-22 17:47:00 +0000

    Added tastypie rest library and its requirements. fixed static file serving on GAE and local. Added basic example of api

--- src/urls.py
+++ src/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from tastypie.api import Api
+from blokus.api import *
 admin.autodiscover()
 
 handler500 = 'djangotoolbox.errorviews.server_error'
+rest_api = Api(api_name='rest')
+rest_api.register(UserResource())
+
+
 
 urlpatterns = patterns('',
-    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
-    ('^$', 'django.views.generic.simple.direct_to_template',
-     {'template': 'game.html'}),
-	url(r'^lobby/', 'django.views.generic.simple.direct_to_template', {'template':'lobby.html'}),
-    url(r'^admin/', include(admin.site.urls)),
+	('^_ah/warmup$', 'djangoappengine.views.warmup'),
+	('^$', 'django.views.generic.simple.direct_to_template', {'template': 'game.html'}),
+	(r'^api/', include(rest_api.urls)),
+	(r'^lobby/', 'django.views.generic.simple.direct_to_template', {'template':'lobby.html'}),	
+	(r'^admin/', include(admin.site.urls)),
 )
