4863337318e12689c36927136599528f671bf534
Peter West <peter.g.west@gmail.com>
2011-12-06 22:01:24 +0000

    Removed functions from global scope. Now using jQuery event handling. Bonus: signin/profile panel now animated. Signin/profile panel now only disappears after mouse has left for a set duration. 3 players -> 4 players on lobby. Dynamic view switcher change. Bonus. View change animation. Better option select code on lobby. Replaced lots of a hrefs from "#" (this is incorrect because it navigates to top of page normally, and in out case of routing, to the front page) to "javascript:;". Logo links to home page.

--- src/blokus/static/js/blokus.js
+++ src/blokus/static/js/blokus.js
@@ -1,54 +1,6 @@
-﻿var showProfileMenu = function() {
-	$("#profileMenu").show();
-	$("#profileButton").addClass("sel");
-}
-
-var hideProfileMenu = function() {
-	$("#profileMenu").hide();
-	$("#profileButton").removeClass("sel");
-}
-
-var signIn = function(name) {
-	$("#signedOutMenu").hide();
-	$("#signedInMenu").show();
-	$("#profileButton").html(name + " ▾");
-	positionProfileMenu();
-	showProfileMenu();
-}
-
-var signOut = function() {
-	$("#signedInMenu").hide();
-	$("#signedOutMenu").show();
-	$("#profileButton").html("Sign in  ▾");
-	positionProfileMenu();
-	showProfileMenu();
-}
-
-var selectMode = function(mode) {
-	var buttons = $(".modelist li");
-	buttons.removeClass("sel");
-	buttons.eq(mode).addClass("sel");
-
-	if (mode == 3){
-		$("#privatelobby").slideDown("slow");
-	}
-	else {
-		$("#privatelobby").slideUp("slow");
-	}
-}
-
-//Position the profile menu underneath the profile button
-var positionProfileMenu = function() {
-	var profileButton = $("#profileButton");
-	var buttonOffset = profileButton.offset();
-	$("#profileMenu").css({
-		'left': buttonOffset.left - $("#profileMenu").outerWidth() + profileButton.outerWidth(),
-		'top': buttonOffset.top + profileButton.outerHeight()
-	});
-}
-
-// Create the blokus core module
+﻿// Create the blokus core module
 window.blokus = (function ($, _, Backbone, Raphael) {
+	"use strict";
 
 	var DEBUG = true,
 
@@ -57,7 +9,7 @@ window.blokus = (function ($, _, Backbone, Raphael) {
 
 		urls = {
 			user: restRootUrl + "user/",
-			user: restRootUrl + "userProfile/",
+			userProfile: restRootUrl + "userProfile/",
 			game: restRootUrl + "game/",
 			pieceMaster: restRootUrl + "piece-master/"
 		};
@@ -72,7 +24,20 @@ window.blokus = (function ($, _, Backbone, Raphael) {
 
 	$(document).ready(function () {
 
-		var Router = Backbone.Router.extend({
+		// Switch to a different view (lobby, game, help etc)
+		function switchToView (view) {
+			var $oldview = $("#container > div"),
+				// render new view
+				$newview = $(view.render().el);
+			
+			// Fade out old view then remove
+			$oldview.fadeOut(200, function () { $oldview.remove(); });
+			// Fade in new view
+			$("#container").append($newview);
+		}
+
+		// Make a new router
+		new (Backbone.Router.extend({
 			routes: {
 				"": "lobby",
 				"game/:id": "game",
@@ -82,64 +47,76 @@ window.blokus = (function ($, _, Backbone, Raphael) {
 				"profile/:id": "profile"
 			},
 
-			lobby: function () {
-				var view = new blokus.LobbyView();
-				$("#container").html("").append(view.render().el);
-			},
+			lobby: 		function () 	{ switchToView(new blokus.LobbyView()); },
+			game: 		function (id) 	{ switchToView(new blokus.GameView({ id: id })); },
+			help: 		function () 	{ switchToView(new blokus.HelpView()); },
+			register: 	function () 	{ switchToView(new blokus.RegisterView()); },
+			forgot: 	function () 	{ switchToView(new blokus.ForgotView()); },
+			profile: 	function (id) 	{ switchToView(new blokus.ProfileView({ id: id })); }
+		}));
 
-			game: function (id) {
-				var view = new blokus.GameView({ id: id });
-				$("#container").html("").append(view.render().el);
-			},
-
-			help: function () {
-				var view = new blokus.HelpView();
-				$("#container").html("").append(view.render().el);
-			},
-
-			register: function () {
-				var view = new blokus.RegisterView();
-				$("#container").html("").append(view.render().el);
-			},
-
-			forgot: function () {
-				var view = new blokus.ForgotView();
-				$("#container").html("").append(view.render().el);
-			},
-
-			profile: function (id) {
-				var view = new blokus.ProfileView({ id: id });
-				$("#container").html("").append(view.render().el);
-			}
-		});
-
-		new Router();
-
+		// Start backbone history to allow routing
 		Backbone.history.start();
 
-		positionProfileMenu();
+		var $profileButton = $("#profileButton"),
+			$profileMenu = $("#profileMenu"),
+			mouseInProfilePanel = false,
+			panelMouseOutTimeouts = [],
 
+			hideProfileMenu = function () {
+		        $profileMenu.slideUp();
+		        $profileButton.removeClass("sel");
+			},
 
+			positionProfileMenu = function () {
+				var buttonOffset = $profileButton.offset();
 
-		// TEMP HACK
-		if (location.href.indexOf("game") > -1) {
+		        $profileMenu.css({
+	                'left': buttonOffset.left - $profileMenu.outerWidth() + $profileButton.outerWidth(),
+	                'top': buttonOffset.top + $profileButton.outerHeight()
+		        });
+			};
 
+		// Make profile/login panel visible when moving mouse onto profile button or menu
+		$("#profileButton, #profileMenu").mouseover(function() {
+			mouseInProfilePanel = true;
+	        $profileMenu.slideDown();
+	        $profileButton.addClass("sel");
+	        // clear all timeouts
+	        _(panelMouseOutTimeouts).each(clearTimeout);
+	        panelMouseOutTimeouts = [];
+		}).mouseout(function () {
+			mouseInProfilePanel = false;
+			// Close panel 200ms after mouse leaves
+			panelMouseOutTimeouts.push(setTimeout(function () {
+				if (!mouseInProfilePanel) {
+					hideProfileMenu();
+			    }
+			}, 300));
+		});
 
-		} else if (location.href.indexOf("lobby") > -1) {
-			/*var gameList = new blokus.GameList({ games: games });
-			$("#lobbylist-container").append(gameList.render().el);*/
+		$("#profileMenu a").click(hideProfileMenu);
 
-		} else {
-			/*
-			var gameboard = new blokus.GameBoard({ game: games.get(0) });
-			$("#container").append(gameboard.render().el);
-			// Temp gameboard
-			blokus.gameboard = gameboard;
-			//Temp just show that I can draw things
-			data = blokus.pieceMasters.get(3).get("data");
-			shape_set = blokus.drawPiece(50,23,data, gameboard)
-			*/
-		}
+		// Handle sign in
+		$("#profileMenu #signin-button").click(function () {
+			var name = "zanders3";
+			$("#signedOutMenu").hide();
+	        $("#signedInMenu").show();
+	        $("#profileButton").html(name + " ▾");
+	        positionProfileMenu();
+	        return false;
+		});
+
+		// Handle sign out
+		$("#profileMenu #signout-button").click(function () {
+			$("#signedInMenu").hide();
+	        $("#signedOutMenu").show();
+	        $("#profileButton").html("Sign in  ▾");
+	        positionProfileMenu()
+		});
+
+		// When window loads or is resized, Set profile/login panel position to below button 
+		$(window).bind("resize load", positionProfileMenu);
 
 	});
 
