8064e02dc201e4cbbf2eaab60f7d3174ff3abd86
Skinna <js40g09@ecs.soton.ac.uk>
2011-12-16 16:44:34 +0000

    Moved create_user_profile to new file signals.py.

---
+++ src/blokus/signals.py
@@ -0,0 +1,8 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
