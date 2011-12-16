4d089e0ebec261b1acb142b81abf228994448b41
adamscybot <adamscybot@localhost>
2011-12-16 18:54:24 +0000

    temp login as we need to test things now

--- src/blokus/views.py
+++ src/blokus/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from blokus.api import UserResource
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest
+from django.views.decorators.http import require_http_methods
+
+@require_http_methods(["GET"])
+def get_logged_in_user(request):
+	if not request.is_ajax():
+		return HttpResponseBadRequest()
+	if request.user.id is None:
+		return HttpResponseNotFound()
+
+	ur = UserResource()
+	user = ur.obj_get(pk=request.user.id)
+	ur_bundle = ur.build_bundle(obj=user, request=request)
+
+	return HttpResponse(ur.serialize(None, ur.full_dehydrate(ur_bundle), 'application/json'))
--- src/urls.py
+++ src/urls.py
@@ -7,14 +7,18 @@ admin.autodiscover()
 handler500 = 'djangotoolbox.errorviews.server_error'
 rest_api = Api(api_name='rest')
 rest_api.register(UserResource())
+rest_api.register(UserProfileResource())
 rest_api.register(GameResource())
 rest_api.register(PieceMasterResource())
 rest_api.register(PieceResource())
 rest_api.register(PlayerResource())
 
 urlpatterns = patterns('',
-	('^_ah/warmup$', 'djangoappengine.views.warmup'),
-	('^$', 'django.views.generic.simple.direct_to_template', {'template': 'game.html'}),
+	(r'^_ah/warmup$', 'djangoappengine.views.warmup'),
+	(r'^get_logged_in_user/$', 'blokus.views.get_logged_in_user'),
+	(r'^login/$', 'django.contrib.auth.views.login'),
+	(r'^logout/$', 'django.contrib.auth.views.logout'),
+	(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'game.html'}),
 	(r'^api/', include(rest_api.urls)),
 	(r'^lobby/', 'django.views.generic.simple.direct_to_template', {'template':'lobby.html'}),	
 	(r'^admin/', include(admin.site.urls)),
