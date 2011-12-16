7898fc4b538a69f3021f74ec5bbdcb60472dfd04
Skinna <js40g09@ecs.soton.ac.uk>
2011-12-16 16:58:49 +0000

    Added database hook to add a move to the Move table whenevr a new Piece is added. Added number_of_moves attribute to Game table.

--- src/blokus/signals.py
+++ src/blokus/signals.py
@@ -1,8 +1,16 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
+from blokus.models import Piece, Move
 
 def create_user_profile(sender, instance, created, **kwargs):
 	if created:
 		UserProfile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
+
+@receiver(pre_save, sender=Piece)
+def record_move(sender, **kwargs):
+	move = Move()
+	move.piece = sender
+	move.move_number = sender.player.game.number_of_moves + 1
