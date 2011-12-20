c3e8143c6091d8e2661e02194c5309c99bc4110a
Skinna <js40g09@ecs.soton.ac.uk>
2011-12-20 19:56:02 +0000

    Progress on garbage-collecting.

--- src/blokus/views.py
+++ src/blokus/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render_to_response
 from blokus.api import UserResource
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
+from datetime import timedelta, datetime
 
 def execute_garbage_collection(request):
+	#The amount of time which a single player may be disconnected for before the game is garbage-collected.
+	TIMEOUT_IN_SECONDS = 60 * 2
+
+	#If any player has been disconnected for longer than TIMEOUT_IN_SECONDS, delete them and the game they were in.
+	for player in Player.objects.all():
+		if (datetime.now - player.last_activity).seconds > TIMEOUT_IN_SECONDS:
+			#do stuff
+
+	#A view must return a "web response".
 	return HttpResponseNotFound('<h1>ZOMG, CRON JOB!</h1>')
 
 @require_http_methods(["GET"])
