ec252f6329c2e17e9ca30bc2d3d19d4548ea192e
Skinna <js40g09@ecs.soton.ac.uk>
2011-12-25 13:45:44 +0000

    Whenever a player places a piece, the last_active field is updated to datetime.now().

--- src/blokus/signals.py
+++ src/blokus/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from blokus.models import UserProfile,Piece, Move
+from datetime import datetime
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -10,9 +11,16 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Piece)
 def record_move(sender, instance, **kwargs):
+	# Construct and save the move.
 	move = Move()
 	move.piece = instance
 	move.move_number = instance.player.game.number_of_moves + 1
 	instance.player.game.number_of_moves = instance.player.game.number_of_moves + 1
 	move.save()
+
+	# Update the players 'last_active' field.
+	instance.player.last_active = datetime.now()
+	instance.player.save()
+
+	# Why is this here?
 	print kwargs
