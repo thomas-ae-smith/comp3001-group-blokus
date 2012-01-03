bc99db9fec47454578994e8cca01fdaccd601b8b
taes1g09 <taes1g09@ecs.soton.ac.uk>
2012-01-03 21:42:18 +0000

    Reworked guest to use GAE's cron.yaml instead of django-cron. Warning: have removed default settings for guest. This -r probably won't run.

--- src/guest/__init__.py
+++ src/guest/__init__.py
@@ -8,6 +8,6 @@ user accounts are created so that they can test the site out.
 # Set up settings for this application.
 # They default to those in guest.default_settings.py but are
 # overridden by DJANGO_SETTINGS_MODULE.
-from gyroid_utils.conf import Settings, module_from_env_var
+#from gyroid_utils.conf import Settings, module_from_env_var
 from . import default_settings
-settings = Settings([module_from_env_var(), default_settings])
+#settings = Settings([module_from_env_var(), default_settings])
--- src/settings.py
+++ src/settings.py
@@ -4,6 +4,7 @@
 from djangoappengine.settings_base import *
 
 import os
+import datetime
 
 # Activate django-dbindexer for the default database
 DATABASES['native'] = DATABASES['default']
@@ -60,6 +61,19 @@ GOOGLE_OAUTH2_CLIENT_ID      = ''
 GOOGLE_OAUTH2_CLIENT_SECRET  = ''
 
 SOCIAL_AUTH_EXPIRATION = 'expires'
+
+# The username of a guest user for display purposes.
+GUEST_USER_NAME = 'Guest'
+# A dummy password for guests.
+GUEST_PASSWORD = 'whatever'
+# A dummy email for guests.
+GUEST_EMAIL = ''
+
+# The amount of time after which an unused guest user can be deleted.
+GUEST_DELETE_TIME = datetime.timedelta(hours = 72)
+# Frequency with which to delete old guests in seconds if using the
+# django-cron application to do this.
+GUEST_DELETE_FREQUENCY = 86400
 
 MIDDLEWARE_CLASSES = (
     # This loads the index definitions, so it has to come first
