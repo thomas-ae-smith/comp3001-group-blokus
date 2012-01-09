09ec2eb7f2e52f908b4c3d7d6210895aa60245b8
Alex Parker <ajp3g08@ecs.soton.ac.uk>
2012-01-09 14:58:15 +0000

    Profile screen

--- src/blokus/static/js/ProfileView.js
+++ src/blokus/static/js/ProfileView.js
@@ -1,2 +1,16 @@
 blokus.ProfileView = Backbone.View.extend({
+	render: function () {
+		var this_ = this,
+			template = _.template($('#profile-template').html());
+		var userProfile = blokus.user.get("userprofile");
+		var dateJoined = new Date(blokus.user.get("date_joined"));
+		var dateText = dateJoined.getDate() + "/" + (dateJoined.getMonth()+1) + "/" + dateJoined.getFullYear() + " " + dateJoined.getHours() + ":" + dateJoined.getMinutes();
+		$(this.el).html(template({
+			username: blokus.user.get("username"),
+			joined: dateText,
+			wins: userProfile.wins.toString(),
+			losses: userProfile.losses.toString()
+		}));
+		return this;
+	}
 });
