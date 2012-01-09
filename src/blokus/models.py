8015946654dfff6d652247f562267c6817869452
adamscybot <adamscybot@localhost>
2012-01-09 19:06:28 +0000

    adding debugging logging

--- src/blokus/models.py
+++ src/blokus/models.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 import hashlib
+import logging
 
 _colour_regex = r"^(blue|yellow|red|green)$"
 
