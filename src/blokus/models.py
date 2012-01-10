bb45c044000193d9eaba6be897212f46291f9ee5
Skinna <js40g09@ecs.soton.ac.uk>
2012-01-10 15:53:43 +0000

    Have to go to a lecture - commented out buggy code for a bit.

--- src/blokus/models.py
+++ src/blokus/models.py
@@ -268,22 +268,31 @@ class Move(models.Model):
 ############
 # SIGNALS  #
 ############
-
-#@receiver(pre_save, sender=UserProfile)
+#
+## If a UserProfile changes its status, all Player objects associated with the Use are removed.
+#@receiver(post_save, sender=UserProfile)
 #def delete_players_on_status_change(sender, instance, created, **kwargs):
-	#if not created and 
-
-# If a Player object is created for a user with existing Player objects,
-# and the new object is attached to a different game to the old object(s),
-# delete the old Player object.
-@receiver(pre_save, sender=Player)
-def delete_players_on_new_game(sender, instance, **kwargs):
-	oldPlayers = Player.objects.filter(id=instance.id)
-	if len(oldPlayers) > 0 and instance.game_id != oldPlayers[0].game.id:
-		for player in oldPlayers:
-			player.delete()
-			player.save()
-
+#	if not created:
+#		oldRecord = UserProfile.objects.get(id=instance.id)
+#		if (oldRecord.id == instance.id) and (oldRecord.status != newRecord.status):
+#			import sys
+#			print >>sys.err, "Removing players for user: " + instance.user.username + " (delete_players_on_status_change)"
+#			for player in instance.user.player_set.all():
+#				player.delete()
+#				player.save()
+#
+## If a Player object is created for a user with existing Player objects,
+## and the new object is attached to a different game to the old object(s),
+## delete the old Player object.
+#@receiver(pre_save, sender=Player)
+#def delete_players_on_new_game(sender, instance, **kwargs):
+#	oldPlayers = Player.objects.filter(id=instance.id)
+#	if len(oldPlayers) > 0 and instance.game_id != oldPlayers[0].game.id:
+#		print >>sys.err, "Removing players for user: " + instance.user.username + " (delete_players_on_new_game)"
+#		for player in oldPlayers:
+#			player.delete()
+#			player.save()
+#
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
 	from blokus.models import UserProfile
